d0ba1d870494dd399cbf890effc36ee64ccfc135
Mikhail Mefodyev <367394@niuitmo.ru>
2023-03-19 18:15:29 +0300

    Implemented proper iterator

--- include/circular_buffer.h
+++ include/circular_buffer.h
@@ -10,16 +10,17 @@ class BufferIterator {
 public:
     using value_type        = typename Buffer::value_type;
     using reference         = value_type&;
-    using pointer           = value_type*;
     using size_type         = typename Buffer::size_type;
+    using pointer           = value_type*;
     using difference_type   = typename Buffer::difference_type;
     using iterator_category = std::random_access_iterator_tag;
 public:
-    BufferIterator(pointer ptr, pointer start, size_type size)
+    BufferIterator(pointer ptr, pointer data_base, pointer data_begin, size_type size)
         : ptr_(ptr)
-        , start_(start)
+        , data_base_(data_base)
+        , data_begin_(data_begin)
         , size_(size)
-        , position_(ptr - start)
+        , position_(ptr - data_base)
     {}
 public:
     reference operator*() {
@@ -48,10 +49,10 @@ public:
         ++ptr_;
         ++position_;
 
-        size_type distance = ptr_ - start_;
+        size_type distance = ptr_ - data_base_;
 
-        if (distance == size_ + 1) {
-            ptr_ = start_;
+        if (distance == size_) {
+            ptr_ = data_base_;
             position_ = 0;
         }
 
@@ -69,10 +70,10 @@ public:
         --ptr_;
         --position_;
 
-        int64_t distance = ptr_ - start_;
+        int64_t distance = ptr_ - data_base_;
 
         if (distance == -1) {
-            ptr_ = start_ + size_;
+            ptr_ = data_base_ + size_;
             position_ = size_;
         }
 
@@ -86,27 +87,38 @@ public:
         return iterator;
     }
 
-    BufferIterator operator+(size_type n) {
+    BufferIterator operator+(size_type n) const {
         n %= size_;
 
         if (size_ - position_ >= n) {
-            return BufferIterator(ptr_ + n, start_, size_);
+            return BufferIterator(ptr_ + n, data_base_, data_begin_, size_);
         }
 
         n -= size_ - position_;
 
-        return BufferIterator(start_ + n - 1, start_, size_);
+        return BufferIterator(data_base_ + n - 1, data_base_, data_begin_, size_);
     }
 
-    BufferIterator operator-(size_type n) {
+    BufferIterator operator-(size_type n) const {
         n %= size_;
 
         return (*this + (size_ - n));
     }
 
-    size_type operator-(const BufferIterator& rhs) {
-        // ! changes needed
-        return ptr_ - rhs.ptr_;
+    int64_t operator-(const BufferIterator& rhs) const {
+        int64_t distance = ptr_ - rhs.ptr_;
+
+        if (
+        (data_begin_ <= ptr_ && data_begin_ <= rhs.ptr_) ||
+        (data_begin_ > ptr_ && data_begin_ > rhs.ptr_)
+        ) {
+            return distance;
+        }
+
+        int64_t abstract_distance = std::max(ptr_, rhs.ptr_) - std::min(ptr_, rhs.ptr_);
+        int sign = (ptr_ < rhs.ptr_ ? 1 : -1);
+
+        return sign * (size_ - abstract_distance);
     }
 
     friend BufferIterator& operator+(size_type lhs, const BufferIterator& rhs) {
@@ -122,27 +134,24 @@ public:
     }
 
     bool operator>(const BufferIterator& other) const {
-        // ! changes needed
-        return ptr_ > other.ptr_;
+        return (*this - other) > 0;
     }
 
     bool operator<(const BufferIterator& other) const {
-        // ! changes needed
-        return ptr_ < other.ptr_;
+        return (*this - other) < 0;
     }
 
     bool operator>=(const BufferIterator& other) const {
-        // ! changes needed
-        return ptr_ >= other.ptr_;
+        return (*this - other) >= 0;
     }
 
     bool operator<=(const BufferIterator& other) const {
-        // ! changes needed
-        return ptr_ <= other.ptr_;
+        return (*this - other) <= 0;
     }
 private:
     pointer ptr_;
-    pointer start_;
+    pointer data_base_;
+    pointer data_begin_;
     size_type size_;
     size_type position_;
 };
@@ -157,35 +166,38 @@ public:
     using reference        = value_type&;
     using const_reference  = const value_type&;
     using iterator         = BufferIterator<CircularBuffer<T>>;
-    using const_iterator   = BufferIterator<CircularBuffer<const T>>;
+    using const_iterator   = BufferIterator<const CircularBuffer<T>>;
     using difference_type  = typename Allocator::difference_type;
     using size_type        = typename Allocator::size_type;
 public:
     CircularBuffer()
         : capacity_(0)
+        , real_capacity_(1)
         , size_(0)
         , start_pos_(0)
         , end_pos_(0)
     {
-        data_ = alloc_.allocate(1);
+        data_ = alloc_.allocate(real_capacity_);
     }
 
     CircularBuffer(size_type capacity)
         : capacity_(capacity)
+        , real_capacity_(capacity + 1)
         , size_(0)
         , start_pos_(0)
         , end_pos_(0)
     {
-        data_ = alloc_.allocate(capacity_ + 1);
+        data_ = alloc_.allocate(real_capacity_);
     }
 
     CircularBuffer(size_type size, const_reference fill_with)
         : capacity_(size)
+        , real_capacity_(size + 1)
         , size_(size)
         , start_pos_(0)
         , end_pos_(size)
     {
-        data_ = alloc_.allocate(capacity_ + 1);
+        data_ = alloc_.allocate(real_capacity_);
 
         for (size_type i = 0; i < size; ++i) {
             data_[i] = fill_with;
@@ -203,9 +215,10 @@ public:
         }
 
         capacity_ = size_;
+        real_capacity_ = size_ + 1;
         start_pos_ = 0;
         end_pos_ = size_;
-        data_ = alloc_.allocate(capacity_ + 1);
+        data_ = alloc_.allocate(real_capacity_);
 
         size_type current_index = start_pos_;
 
@@ -218,11 +231,12 @@ public:
 
     CircularBuffer(const std::initializer_list<value_type>& init_list)
         : capacity_(init_list.end() - init_list.begin())
+        , real_capacity_(init_list.end() - init_list.begin() + 1)
         , size_(init_list.end() - init_list.begin())
         , start_pos_(0)
         , end_pos_(init_list.end() - init_list.begin())
     {
-        data_ = alloc_.allocate(capacity_ + 1);
+        data_ = alloc_.allocate(real_capacity_);
         
         auto current = init_list.begin();
 
@@ -234,13 +248,14 @@ public:
 
     CircularBuffer(const CircularBuffer<value_type, Allocator>& other)
         : capacity_(other.capacity_)
+        , real_capacity_(other.real_capacity_)
         , size_(other.size_)
         , start_pos_(other.start_pos_)
         , end_pos_(other.end_pos_)
     {
-        data_ = alloc_.allocate(capacity_ + 1);
+        data_ = alloc_.allocate(real_capacity_);
         
-        for (size_type i = 0; i < capacity_ + 1; ++i) {
+        for (size_type i = 0; i < real_capacity_; ++i) {
             data_[i] = other.data_[i];
         }
     }
@@ -250,15 +265,16 @@ public:
             return *this;
         }
 
-        alloc_.deallocate(data_, capacity_ + 1);
+        alloc_.deallocate(data_, real_capacity_);
 
         capacity_ = other.capacity_;
+        real_capacity_ = other.real_capacity_;
         size_ = other.size_;
-        data_ = alloc_.allocate(capacity_ + 1);
+        data_ = alloc_.allocate(real_capacity_);
         start_pos_ = other.start_pos_;
         end_pos_ = other.end_pos_;
 
-        for (size_type i = 0; i < capacity_ + 1; ++i) {
+        for (size_type i = 0; i < real_capacity_; ++i) {
             data_[i] = other.data_[i];
         }
 
@@ -266,11 +282,12 @@ public:
     }
 
     CircularBuffer& operator=(const std::initializer_list<value_type>& other) {
-        alloc_.deallocate(data_, capacity_ + 1);
+        alloc_.deallocate(data_, real_capacity_);
 
         capacity_ = other.end() - other.begin();
+        real_capacity_ = capacity_ + 1;
         size_ = capacity_;
-        data_ = alloc_.allocate(capacity_ + 1);
+        data_ = alloc_.allocate(real_capacity_);
         start_pos_ = 0;
         end_pos_ = size_;
 
@@ -283,27 +300,61 @@ public:
     }
 
     ~CircularBuffer() {
-        alloc_.deallocate(data_, capacity_ + 1);
+        alloc_.deallocate(data_, real_capacity_);
     }
 public:
     iterator begin() {
-        // ! changes needed
-        return iterator(data_ + start_pos_, data_, capacity_);
+        return iterator(
+            data_ + start_pos_,
+            data_,
+            data_ + start_pos_,
+            real_capacity_
+        );
     }
 
     iterator end() {
-        // ! changes needed
-        return iterator(data_ + end_pos_, data_, capacity_);
+        return iterator(
+            data_ + end_pos_,
+            data_,
+            data_ + start_pos_,
+            real_capacity_
+        );
+    }
+
+    const_iterator begin() const {
+        return const_iterator(
+            data_ + start_pos_,
+            data_,
+            data_ + start_pos_,
+            real_capacity_
+        );
+    }
+
+    const_iterator end() const {
+        return const_iterator(
+            data_ + end_pos_,
+            data_,
+            data_ + start_pos_,
+            real_capacity_
+        );
     }
 
     const_iterator cbegin() const {
-        // ! changes needed
-        return const_iterator(data_ + start_pos_, data_, capacity_);
+        return const_iterator(
+            data_ + start_pos_,
+            data_,
+            data_ + start_pos_,
+            real_capacity_
+        );
     }
 
     const_iterator cend() const {
-        // ! changes needed
-        return const_iterator(data_ + end_pos_, data_, capacity_);
+        return const_iterator(
+            data_ + end_pos_,
+            data_,
+            data_ + start_pos_,
+            real_capacity_
+        );
     }
 
     bool operator==(const CircularBuffer& other) const {
@@ -351,6 +402,7 @@ public:
     }
 private:
     size_type capacity_;
+    size_type real_capacity_;
     size_type size_;
     value_type* data_;
     Allocator alloc_;
@@ -359,6 +411,6 @@ private:
     size_type end_pos_;
 private:
     size_type GetNextPosition(size_type pos) {
-        return (pos + 1) % (capacity_ + 1);
+        return (pos + 1) % (real_capacity_);
     }
 };
